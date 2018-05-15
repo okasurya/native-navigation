d890a949939e5f6449b397f80422118f6ef1b02f
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2018-05-15 09:42:39 +0700

    adjust closure type barbuttonitem (#2)

--- lib/ios/native-navigation/UIBarButtonItem+addAction.swift
+++ lib/ios/native-navigation/UIBarButtonItem+addAction.swift
@@ -11,7 +11,7 @@ import Foundation
 
 public class ClosureWrapper : NSObject {
   let _callback : () -> Void
-  init(callback : @escaping (Void) -> Void) {
+  init(callback : @escaping () -> Void) {
     _callback = callback
   }
 
@@ -24,7 +24,7 @@ public class ClosureWrapper : NSObject {
 var AssociatedClosure: UInt8 = 0
 
 extension UIControl {
-  func nn_addAction(forControlEvents events: UIControlEvents, withCallback callback: @escaping (Void) -> Void) {
+  func nn_addAction(forControlEvents events: UIControlEvents, withCallback callback: @escaping () -> Void) {
     let wrapper = ClosureWrapper(callback: callback)
     addTarget(wrapper, action: #selector(ClosureWrapper.invoke), for: events)
     objc_setAssociatedObject(self, &AssociatedClosure, wrapper, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
